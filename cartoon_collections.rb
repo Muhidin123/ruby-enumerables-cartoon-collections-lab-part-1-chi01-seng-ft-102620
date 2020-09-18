6ce334ba36f46237c463a858b7e5361f6affc3d5
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-18 15:51:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index() do |elem , ix+1|
+  array.each_with_index() do |elem , ix|
     puts "#{ix}.#{elem}"
   end
 end
